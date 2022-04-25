9b3df4f34db9f2cd3bfbacbe26a3313a9e3e35f4
lijokufgh <ilygj@mail.ru>
2022-04-25 09:10:56 +0500

    Добавьте файлы проекта.

---
+++ Lab3_1/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Program
+{
+    public class Lab_1 // Создаём основной класс.
+    {
+        public double log; // Переменая для логорифма.
+        public double osn; // Переменая для основания.
+        public int okr; // Переменая для округления.
+
+        public Lab_1() // Заполняем переменные. Выводиться при вызови объекта.
+        {
+            Console.WriteLine("Введите логорифм: ");
+            log = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите основани: ");
+            osn = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите до скольки знаков округлять: ");
+            okr = int.Parse(Console.ReadLine());
+        }
+    }
+
+    public static void Main() // Основная функция.
+    {
+        Lab_1 lab1 = new Lab_1(); // Создание объекта.
+        Console.WriteLine(Math.Round(Math.Log(lab1.log, lab1.osn), lab1.okr)); // Проводим логарифмирование и округление.
+    }
+}
